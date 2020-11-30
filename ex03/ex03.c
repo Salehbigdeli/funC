a8265d3965614501dd745f4e99923ecaae218a14
Aliakbar Salehbigdeli <al.ak.saleh@gmail.com>
2020-11-30 06:30:49 +0330

    ex3

---
+++ ex03/ex03.c
@@ -0,0 +1,12 @@
+//
+// Created by ali on 11/30/20.
+//
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int age;
+    int height = 234;
+    printf("height is %d and age is %d\n", height, age);
+    return 0;
+}
